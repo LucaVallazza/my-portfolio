40874c862f28bb54d905e0d8e816668383161552
Luca <lucavallazza@gmail.com>
2024-07-16 19:21:23 -0300

    general advances

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
 import { useRef, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Pana from './pana';
-import Header from './header';
+import Header from './components/header';
+import Skills from './components/skills';
 
 function App() {
 
-  const [pana, setPana] = useState<any[]>([]);
 
-  const name = useRef(null)
-  const edad = useRef(null)
-  const ig = useRef(null)
-
-
-  const onclick = () => {
-    
-
-
-    console.log(pana)
-
-  }
 
   return (
     <main className='h-[100vh]'>
       <Header></Header>
+      <Skills></Skills>
 
     </main>
   )
